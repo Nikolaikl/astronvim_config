5eeb4c5658c7ecad0d2b39f4f3abbddd5dc48918
Nikolaikl <Nikolai.Klassen@live.com>
2023-11-16 14:32:10 +0100

    Adding packages + Default colorscheme

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "lemminx",
+        "pyright",
+        "python-ls-server",
+        "ruff-lsp",
+        "marksman",
+
       })
     end,
   },
@@ -18,8 +24,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
+        "yapf"
       })
     end,
   },
@@ -29,7 +36,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
