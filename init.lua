f1d354eb97361ef1ea7a32c4683b78416b46bc2d
Nikolaikl <Nikolai.Klassen@live.com>
2023-11-17 14:03:05 +0100

    Adding new logo + theme fix

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astroMars",
+  colorscheme = "astromars",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
